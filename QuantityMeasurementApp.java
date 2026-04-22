e4c5c5aead7e86074f815db0b0739b39e12d18e3
neel597 <neelaka597@gmail.com>
2026-04-22 09:45:40 +0530

    UC7: Addition with Target Unit Specification

--- QuantityMeasurementApp.java
+++ QuantityMeasurementApp.java
@@ -58,19 +58,27 @@ public class QuantityMeasurementApp {
             return new QuantityLength(convertedValue, targetUnit);
         }
 
-        // Addition instance method
+        // Addition (UC6): result in unit of first operand
         public QuantityLength add(QuantityLength other) {
+            return add(other, this.unit);
+        }
+
+        // Addition (UC7): result in explicitly specified target unit
+        public QuantityLength add(QuantityLength other, LengthUnit targetUnit) {
             if (other == null) {
                 throw new IllegalArgumentException("Other quantity cannot be null");
+            }
+            if (targetUnit == null) {
+                throw new IllegalArgumentException("Target unit cannot be null");
             }
 
             double thisInFeet = this.unit.toFeet(this.value);
             double otherInFeet = other.unit.toFeet(other.value);
 
             double sumInFeet = thisInFeet + otherInFeet;
-            double sumInTargetUnit = this.unit.fromFeet(sumInFeet);
+            double sumInTargetUnit = targetUnit.fromFeet(sumInFeet);
 
-            return new QuantityLength(sumInTargetUnit, this.unit);
+            return new QuantityLength(sumInTargetUnit, targetUnit);
         }
 
         @Override
@@ -82,24 +90,22 @@ public class QuantityMeasurementApp {
     // Demo main method
     public static void main(String[] args) {
         QuantityLength feet1 = new QuantityLength(1.0, LengthUnit.FEET);
-        QuantityLength feet2 = new QuantityLength(2.0, LengthUnit.FEET);
-        System.out.println("1 foot + 2 feet = " + feet1.add(feet2)); // 3 FEET
-
         QuantityLength inch12 = new QuantityLength(12.0, LengthUnit.INCH);
-        System.out.println("1 foot + 12 inches = " + feet1.add(inch12)); // 2 FEET
 
-        System.out.println("12 inches + 1 foot = " + inch12.add(feet1)); // 24 INCH
+        System.out.println("1 foot + 12 inches (FEET): " + feet1.add(inch12, LengthUnit.FEET)); // 2 FEET
+        System.out.println("1 foot + 12 inches (INCHES): " + feet1.add(inch12, LengthUnit.INCH)); // 24 INCH
+        System.out.println("1 foot + 12 inches (YARDS): " + feet1.add(inch12, LengthUnit.YARD)); // ~0.667 YARD
 
         QuantityLength yard1 = new QuantityLength(1.0, LengthUnit.YARD);
         QuantityLength feet3 = new QuantityLength(3.0, LengthUnit.FEET);
-        System.out.println("1 yard + 3 feet = " + yard1.add(feet3)); // 2 YARD
+        System.out.println("1 yard + 3 feet (YARDS): " + yard1.add(feet3, LengthUnit.YARD)); // 2 YARD
 
         QuantityLength cm2_54 = new QuantityLength(2.54, LengthUnit.CENTIMETER);
         QuantityLength inch1 = new QuantityLength(1.0, LengthUnit.INCH);
-        System.out.println("2.54 cm + 1 inch = " + cm2_54.add(inch1)); // ~5.08 CENTIMETER
+        System.out.println("2.54 cm + 1 inch (CENTIMETERS): " + cm2_54.add(inch1, LengthUnit.CENTIMETER)); // ~5.08 CM
 
         QuantityLength feet5 = new QuantityLength(5.0, LengthUnit.FEET);
         QuantityLength feetMinus2 = new QuantityLength(-2.0, LengthUnit.FEET);
-        System.out.println("5 feet + (-2 feet) = " + feet5.add(feetMinus2)); // 3 FEET
+        System.out.println("5 feet + (-2 feet) (INCHES): " + feet5.add(feetMinus2, LengthUnit.INCH)); // 36 INCH
     }
 }
